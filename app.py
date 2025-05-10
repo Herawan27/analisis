a4abc8573d11b4a6b7661cafb81d07075ad207ac
Herawan27 <herawanheraone@gmail.com>
2025-05-10 12:54:37 +0700

    yuaaaa

--- app.py
+++ app.py
@@ -143,19 +143,20 @@ if file:
         model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=3)
 
         training_args = TrainingArguments(
-            output_dir='./results',
-            run_name='bert_sentimen_jmo',
-            num_train_epochs=3,
-            per_device_train_batch_size=16,
-            per_device_eval_batch_size=64,
-            warmup_steps=500,
-            weight_decay=0.01,
-            logging_dir='./logs',
-            logging_steps=200,
-            disable_tqdm=False,
-            no_cuda=False,
-            report_to="wandb"
-        )
+    output_dir='./results',
+    run_name='bert_sentimen_jmo',
+    num_train_epochs=3,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=64,
+    warmup_steps=500,
+    weight_decay=0.01,
+    logging_dir='./logs',
+    logging_steps=200,
+    disable_tqdm=False,
+    no_cuda=True,  # <-- ini yang penting!
+    report_to="wandb"
+)
+
 
         trainer = Trainer(
             model=model,
