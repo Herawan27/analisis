5c4896cf62f8f5c36fea9d4b78154f5955e3b0a1
Herawan27 <herawanheraone@gmail.com>
2025-05-13 09:31:41 +0700

    rwe

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def main():
                 X_train, X_test, y_train, y_test = train_test_split(
                     X,
                     y,
-                    test_size=0.3,
+                    test_size=0.2,
                     stratify=y,
                     random_state=42
                 )
